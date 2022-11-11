da4948dc6980694ad99b311c3fe6dcca1fb68a7f
morciu <marianpralea@gmail.com>
2022-11-11 11:02:39 +0200

    created silly contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,19 @@
 export default function loadContact() {
     const element = document.createElement('div');
     element.id = "contentBody";
-    element.innerText = "Contact";
+    
+    const itemDiv = document.createElement('div');
+    itemDiv.className = "description";
+
+    const itemHeader = document.createElement('div');
+    itemHeader.innerText = "Contact";
+    itemDiv.appendChild(itemHeader);
+
+    const itemBody = document.createElement('div');
+    itemBody.innerText = "Don't call us, we'll call you!";
+    itemDiv.appendChild(itemBody);
+
+    element.appendChild(itemDiv);
 
     return element;
 }
